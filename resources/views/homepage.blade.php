ac50b6a2f2f6346b7dede1344c50fea17ed92858
Michele Lorusso <kirito102095@hotmail.it>
2021-06-09 02:49:45 +0200

    comics end

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,53 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h2>homepage</h2>
+    
+    <div class="container">
+
+        <div class="current">
+            <h2>current series</h2>
+        </div>
+
+        {{-- Comics --}}
+        <section class="comics">
+            
+            {{-- single-comic --}}
+            @foreach ($comics_array as $comic)
+            
+            <div class="single-comic">
+
+                <div class="poster">
+                    <img src="{{$comic["thumb"]}}" alt="{{$comic["title"]}}">
+                </div>
+
+                {{-- title-comic --}}
+                <div class="title-comic">
+                    {{$comic["series"]}}
+                </div>
+                {{-- end title-comic --}}
+                
+            </div>
+            
+            @endforeach
+            {{-- single-comic --}}
+
+        </section>
+        {{-- End Comics --}}
+
+        {{-- button --}}
+        <div class="click">
+            <button>LOAD MORE</button>
+        </div>
+        {{-- end button --}}
+
+        {{-- Option --}}
+        <div class="option">
+
+        </div>
+        {{-- End Option --}}
+
+        
+
+    </div>
+
 @endsection
